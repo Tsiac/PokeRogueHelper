babb9c8048cf685019fe4366101448a28e62017a
JackUnthank <jack.unthank@mac.com>
2024-07-08 11:05:12 +0100

    add note

--- chrome-extension/underlay.js
+++ chrome-extension/underlay.js
@@ -8,3 +8,9 @@
 // <div class="grid-cell type-icon" style="position: relative; left: 810px; top: 315px; display: table-cell;">
 // <img src="chrome-extension://lkhfmjndefiiemajaheggjipcgebkmcn/sprites/types/1.png" alt="normal">
 // </div>
+
+// https://bulbapedia.bulbagarden.net/wiki/Damage#Damage_formula
+// Add a number next to the move telling you how many more times it will do damage
+// doesn't need to take into account enemy defence I think that's cheating
+// but a general multiplier, including IV efficiency, STAB, and Typing damage
+// For multibattles put two number (Enemy 1 Value, Enemy 2 Value)
